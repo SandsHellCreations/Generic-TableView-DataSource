24777871c3cd26413cfaa36e453c7f97acbb18a5
SandeepSpider811 <sandeep.kumar811@gmail.com>
2019-07-07 19:02:40 +0530

    Initial Commit

---
+++ GenericTableDataSourceTests/GenericTableDataSourceTests.swift
@@ -0,0 +1,34 @@
+//
+//  GenericTableDataSourceTests.swift
+//  GenericTableDataSourceTests
+//
+//  Created by Sandeep Kumar on 07/07/19.
+//  Copyright © 2019 SandsHellCreations. All rights reserved.
+//
+
+import XCTest
+@testable import GenericTableDataSource
+
+class GenericTableDataSourceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
